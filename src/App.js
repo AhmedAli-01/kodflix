2eec892ea905a0fab1b8671406370e4764952d49
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-06-19 16:50:50 +0100

    Initial commit from Create React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import javascriptLogo from './javascript-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix!!!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img src = {javascriptLogo} alt='JavaScript Logo' />
+
+     <br />  <br />  <br />
+     <div className="header">
+      <div className="headerItems"> <img src="https://img.moviepostershop.com/titanic-movie-poster-1997-1020339699.jpg" alt="Titnic Movie Cover"></img> </div>
+      <div className="headerItems"> <img src="https://i.pinimg.com/236x/1d/42/8f/1d428fc1bb6d2c3528c02753d6b3559e--james-cameron-james-darcy.jpg" alt="Avator Movie Cover"></img>  </div>
+      <div className="headerItems"> <img src="https://i.pinimg.com/originals/50/b8/97/50b8973ec083d449d20b73222f2b2d56.jpg" alt="Titnic Movie Cover"></img>  </div>
+     </div>
+     <div className="header">
+      <div className="headerItems"> <img src="https://img.moviepostershop.com/titanic-movie-poster-1997-1020339699.jpg" alt="Titnic Movie Cover"></img>  </div>
+      <div className="headerItems"> <img src="https://img.moviepostershop.com/titanic-movie-poster-1997-1020339699.jpg" alt="Titnic Movie Cover"></img>  </div>
+      <div className="headerItems"> <img src="https://img.moviepostershop.com/titanic-movie-poster-1997-1020339699.jpg" alt="Titnic Movie Cover"></img>  </div>
+     </div>
     </div>
   );
 }
