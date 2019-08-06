229bf928c3840293d3ddc765af0f32ce10bbb245
DrToil <axmed13@hotmail.com>
2019-08-06 17:17:04 +0100

    got all the picture locally.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,38 @@
 import React from "react";
 import "./App.css";
+import theblindside from "./img/theblindside.jpg";
+import avatar from "./img/avatar.jpg";
+import titanic from "./img/titanic.jpg";
+import blackpanther from "./img/blackpanther.jpg";
+import gameofthrone from "./img/gameofthrone.jpg";
+import hobbsshaw from "./img/hobbsshaw.jpg";
+
+
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
+      <div className="container">
         <div className="headerItems">
-          {" "}
-          <img
-            src="https://www.onthisday.com/images/articles/titanic-the-movie.jpg"
-            alt="Titnic Movie Cover"
-          />{" "}
+          <img src={theblindside} alt="The Blind Side Movie Cover"/>
         </div>
         <div className="headerItems">
-          {" "}
-          <img
-            src="https://i.pinimg.com/originals/0c/c6/e0/0cc6e0968907c35cfc0804703e0aa864.jpg"
-            alt="Avator Movie Cover"
-          />{" "}
+          <img src={avatar} alt="Avator Movie Cover" />
         </div>
-        <div className="headerItems">
-          {" "}
-          <img
-            src="http://img.fruugo.com/product/9/21/77786219_max.jpg"
-            alt="Titnic Movie Cover"
-          />{" "}
+        <div className="headerItems">          
+          <img src={titanic} alt="Titnic Movie Cover" />
         </div>
       </div>
-      <div className="header">
+      <div className="container">
         <div className="headerItems">
-          {" "}
-          <img
-            src="https://cover4theatre.com/wp-content/uploads/2019/01/1025445PL.jpg"
-            alt="Titnic Movie Cover"
-          />{" "}
+          
+          <img src={blackpanther}  alt="black panther cover" />
         </div>
-        <div className="headerItems">
-          {" "}
-          <img
-            src="https://ae01.alicdn.com/kf/HTB11SV6HQyWBuNjy0Fpq6yssXXaN/The-poster-of-the-movie-black-panther-painted-by-the-white-gaoqing-daily.jpg"
-            alt="Titnic Movie Cover"
-          />{" "}
+        <div className="headerItems">          
+          <img src={gameofthrone}  alt="Game of Throne Movie Cover"   />
         </div>
-        <div className="headerItems">
-          {" "}
-          <img
-            src="https://i.pinimg.com/originals/81/80/80/81808030e12ac294c5d43a3905f78274.jpg"
-            alt="Titnic Movie Cover"
-          />{" "}
+        <div className="headerItems">          
+          <img src={hobbsshaw}   alt="Hobbs and Shaw Movie Cover" />
         </div>
       </div>
     </div>
