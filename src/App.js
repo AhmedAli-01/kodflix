6564bb5ba472482b50c3afee6d58018389c418ec
DrToil <axmed13@hotmail.com>
2019-08-06 22:01:17 +0100

    Added over text on each movie cover.

--- src/App.js
+++ src/App.js
@@ -13,26 +13,43 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="headerItems">
+        <div className="containerItem">
           <img src={theblindside} alt="The Blind Side Movie Cover"/>
+          <div className='overlay'>
+            <h1>The Blind Side</h1>
+            </div>
         </div>
-        <div className="headerItems">
+        <div className="containerItem">
           <img src={avatar} alt="Avator Movie Cover" />
+          <div className='overlay'>
+            <h1>Avatar</h1>
+            </div>
         </div>
-        <div className="headerItems">          
+        <div className="containerItem">          
           <img src={titanic} alt="Titnic Movie Cover" />
+          <div className='overlay'>
+            <h1>Titanic</h1>
+            </div>         
         </div>
       </div>
       <div className="container">
-        <div className="headerItems">
-          
+        <div className="containerItem">          
           <img src={blackpanther}  alt="black panther cover" />
+          <div className='overlay'>
+            <h1>Black Panther</h1>
+            </div>
         </div>
-        <div className="headerItems">          
-          <img src={gameofthrone}  alt="Game of Throne Movie Cover"   />
+        <div className="containerItem">          
+          <img src={gameofthrone}  alt="Game of Throne Movie Cover" />
+          <div className='overlay'>
+            <h1>Game of Thron</h1>
+            </div>
         </div>
-        <div className="headerItems">          
+        <div className="containerItem">          
           <img src={hobbsshaw}   alt="Hobbs and Shaw Movie Cover" />
+          <div className='overlay'>
+            <h1>Hobbs & Shaw</h1>
+            </div>
         </div>
       </div>
     </div>
