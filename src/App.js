a7ec028de05bd654409a2f83b1b3d324aa6d0cd7
DrToil <axmed13@hotmail.com>
2019-08-08 18:06:13 +0100

    Added img link to gallery and downloaded react router.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Gallery from "./Gallery";
+import Details from "./Details";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-       <Gallery />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Gallery} />
+        <Route exact path="/details" component={Details} />
+      </div>
+    </Router>
   );
 }
 export default App;
--- src/Stack.js
+++ src/Stack.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 export default function Stack(prop) {
     return (
+      <Link to='/details'>
       <div className="item">
         <img src={prop.logo} alt={`${prop.name} logo`} />
         <div className="overlay">
           <h1>{prop.name}</h1>
         </div>
       </div>
+      </Link>
     );
 }
