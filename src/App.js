fa14efad4e23150c3d359cd7f319bcea62f3557c
DrToil <axmed13@hotmail.com>
2019-08-19 09:56:21 +0100

    removed LINK as it was not used in the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Gallery from "./Gallery";
 import Details from "./Details";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
