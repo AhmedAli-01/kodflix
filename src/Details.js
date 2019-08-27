7b9df9c33e6481bf75bfcb706ae98b77da1329bc
DrToil <axmed13@hotmail.com>
2019-08-27 16:16:01 +0100

    Details.js componend switched to Class component.

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 
 
-export default function Details() {
-    return (
-        <>
-        <h1>Hello, this will be the details page for each Movie & TV show </h1>
-        <Link to='/'>  Back to home  </Link>
-        </>
+export default class Details extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            welcomeMsg : 'Hello, this will be the details page for each Movie & TV show'
+        };
+    }
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState ( {
+                welcomeMsg: 'Huraaaah!!! The title changes after 3 seconds -:)'
+            });
+        }, 3000);
+    }
 
-    );
+    render () {
+        return (
+            <>
+                <h1>{this.state.welcomeMsg}</h1>
+                <Link to='/'>  Back to home  </Link>
+            </>
+        );
+    }
 }
