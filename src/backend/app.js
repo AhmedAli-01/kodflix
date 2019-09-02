21939cc0fb35d4f5d793165f26786cc4ca8b2bc1
DrToil <you@example.com>
2019-09-02 16:36:33 +0100

    get both front and back end running in parallel

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const shows = require('./shows')
 const app = express()
-const port = 7777
+const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/rest/shows', (req, res) => res.send(shows()));
